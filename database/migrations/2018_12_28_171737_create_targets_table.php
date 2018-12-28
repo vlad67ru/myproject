6f992dd8b8788f1b179b6ef111d8944e3aead0c5
vlad67ru <vlad.lab@mail.ru>
2018-12-28 20:21:37 +0300

    создал модель и миграцию для неё

---
+++ database/migrations/2018_12_28_171737_create_targets_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTargetsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('targets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('description');
+            $table->string('time');
+            $table->string('completed');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('targets');
+    }
+}
